7dc82305c121a23a072027ff8c10abef5d17afc7
webhead404 <kvvincentvalentine@gmail.com>
2022-04-08 16:11:30 -0500

    Update start-pneuma.ps1

--- ESXi/ansible/roles/windows10/files/start-pneuma.ps1
+++ ESXi/ansible/roles/windows10/files/start-pneuma.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "C:\Pneuma\pneuma-windows.exe" -ArgumentList "-address 192.168.56.73:2323 -contact tcp -name pneuma-windows -range thremulation"
+Start-Process -FilePath "C:\Pneuma\pneuma-windows.exe" -ArgumentList "-address 192.168.56.13:2323 -contact tcp -name pneuma-windows -range thremulation"
